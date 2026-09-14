95d7108dd308390a482dcad5d6eed0311379b466
praveennahar <praveennahar00@gmail.com>
2026-09-14 21:56:03 +0530

    feat: email login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   stale_when_importmap_changes
+
+  before_action :authenticate_user!, unless: :devise_controller?
+
+  def after_sign_in_path_for(_resource)
+    trips_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "trips#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :trips, only: %i[index show] do
+    resources :holds, only: :create
+  end
+
+  resources :holds, only: :show, param: :token do
+    resources :bookings, only: :create
+  end
+
+  resources :bookings, only: %i[index show], param: :pnr do
+    member do
+      post :cancel
+      get :reschedule, action: :reschedule_form
+      post :reschedule
+    end
+  end
 end
